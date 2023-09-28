2032ad2810a97d8a9261f12e208661ba1d7b920f
choiseunghyeon <ket8780@gmail.com>
2023-09-28 20:45:58 +0900

    added Carousel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import Header from "../../components/Header"
-import Footer from "../../components/Footer"
-import PostList from "../../components/PostList"
+import Header from "components/Header"
+import Footer from "components/Footer"
+import PostList from "components/PostList"
+import Carousel from "components/Carousel"
 
 function Home() {
   return (
     <div>
       <Header />
+      <Carousel />
       <PostList />
       <Footer />
     </div>
